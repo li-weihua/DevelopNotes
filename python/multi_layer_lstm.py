20ef7a54c9c1da4f69e6b7f97cdc83e2a4a06d83
li-weihua <7219159+li-weihua@users.noreply.github.com>
2024-10-21 21:49:22 +0800

    update lstm op

--- python/multi_layer_lstm.py
+++ python/multi_layer_lstm.py
@@ -1,88 +1,79 @@
+from collections import OrderedDict
+from typing import Optional
+
 import torch
-import torch.nn as nn
-
-torch.set_grad_enabled(False)
-
-torch.manual_seed(1)
-
-seq_len = 22
-batch_size = 7
-input_size = 10
-hidden_size = 20
-num_layers = 5
-
-x = torch.randn(seq_len, batch_size, input_size)
-
-rnn = nn.LSTM(input_size, hidden_size, num_layers, batch_first=False, bidirectional=False)
-
-y0, (h0, c0) = rnn.forward(x)
 
 
-wi = []
-wh = []
-bias = []
+class MyLSTM:
+    def __init__(self, input_size: int, hidden_size: int, num_layers: int, state_dict: OrderedDict):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
 
-st = rnn.state_dict()
+        self.wi = []
+        self.wh = []
+        self.bias = []
 
-for layer_id in range(num_layers):
-    wi.append(st[f"weight_ih_l{layer_id}"].T)
-    wh.append(st[f"weight_hh_l{layer_id}"].T)
-    bias.append(st[f"bias_ih_l{layer_id}"] + st[f"bias_hh_l{layer_id}"])
+        for layer_id in range(num_layers):
+            self.wi.append(state_dict[f"weight_ih_l{layer_id}"].T)
+            self.wh.append(state_dict[f"weight_hh_l{layer_id}"].T)
+            self.bias.append(state_dict[f"bias_ih_l{layer_id}"] + state_dict[f"bias_hh_l{layer_id}"])
 
-y1 = torch.zeros(seq_len, batch_size, hidden_size)
-hn = torch.zeros(num_layers, batch_size, hidden_size)
-cn = torch.zeros(num_layers, batch_size, hidden_size)
+    def forward(self, x, h_0: Optional[torch.Tensor] = None, c_0: Optional[torch.Tensor] = None):
+        seq_len, batch_size, input_size = x.shape
 
-for t in range(seq_len):
-    # layer 0
-    ifgo = torch.matmul(x[t], wi[0]) + torch.matmul(hn[0], wh[0]) + bias[0]
+        assert input_size == self.input_size
 
-    i = ifgo[:, 0: hidden_size]
-    f = ifgo[:, hidden_size : hidden_size * 2]
-    g = ifgo[:, hidden_size * 2 : hidden_size * 3]
-    o = ifgo[:, hidden_size * 3 : hidden_size * 4]
+        if h_0 is None:
+            h_n = torch.zeros(self.num_layers, batch_size, self.hidden_size, device=x.device, dtype=x.dtype)
+        else:
+            h_n = h_0.clone()
 
-    i = torch.sigmoid(i)
-    f = torch.sigmoid(f)
-    g = torch.tanh(g)
-    o = torch.sigmoid(o)
+        if c_0 is None:
+            c_n = torch.zeros(self.num_layers, batch_size, self.hidden_size, device=x.device, dtype=x.dtype)
+        else:
+            c_n = c_0.clone()
 
-    cn[0] = f * cn[0] + i * g
-    hn[0] = o * torch.tanh(cn[0])
+        out = torch.empty(seq_len, batch_size, self.hidden_size, device=x.device, dtype=x.dtype)
 
-    # layer 1
-    for layer_id in range(1, num_layers):
-        ifgo = torch.matmul(hn[layer_id-1], wi[layer_id]) + torch.matmul(hn[layer_id], wh[layer_id]) + bias[layer_id]
+        for t in range(seq_len):
+            # layer 0
+            ifgo = torch.matmul(x[t], self.wi[0]) + torch.matmul(h_n[0], self.wh[0]) + self.bias[0]
 
-        i = ifgo[:, 0: hidden_size]
-        f = ifgo[:, hidden_size : hidden_size * 2]
-        g = ifgo[:, hidden_size * 2 : hidden_size * 3]
-        o = ifgo[:, hidden_size * 3 : hidden_size * 4]
+            i = ifgo[:, 0 : self.hidden_size]
+            f = ifgo[:, self.hidden_size : self.hidden_size * 2]
+            g = ifgo[:, self.hidden_size * 2 : self.hidden_size * 3]
+            o = ifgo[:, self.hidden_size * 3 : self.hidden_size * 4]
 
-        i = torch.sigmoid(i)
-        f = torch.sigmoid(f)
-        g = torch.tanh(g)
-        o = torch.sigmoid(o)
+            i = torch.sigmoid(i)
+            f = torch.sigmoid(f)
+            g = torch.tanh(g)
+            o = torch.sigmoid(o)
 
-        cn[layer_id] = f * cn[layer_id] + i * g
-        hn[layer_id] = o * torch.tanh(cn[layer_id])
+            c_n[0] = f * c_n[0] + i * g
+            h_n[0] = o * torch.tanh(c_n[0])
 
+            # layer 1
+            for layer_id in range(1, self.num_layers):
+                ifgo = (
+                    torch.matmul(h_n[layer_id - 1], self.wi[layer_id])
+                    + torch.matmul(h_n[layer_id], self.wh[layer_id])
+                    + self.bias[layer_id]
+                )
 
-    y1[t] = hn[num_layers-1].clone()
+                i = ifgo[:, 0 : self.hidden_size]
+                f = ifgo[:, self.hidden_size : self.hidden_size * 2]
+                g = ifgo[:, self.hidden_size * 2 : self.hidden_size * 3]
+                o = ifgo[:, self.hidden_size * 3 : self.hidden_size * 4]
 
+                i = torch.sigmoid(i)
+                f = torch.sigmoid(f)
+                g = torch.tanh(g)
+                o = torch.sigmoid(o)
 
-# check outputs
-print(f"ref out : {y0.shape}, {y0.min()}, {y0.max()}")
-print(f"cpp out : {y1.shape}, {y1.min()}, {y1.max()}")
-print(f"diff    : {(y1-y0).abs().max()}")
-print()
+                c_n[layer_id] = f * c_n[layer_id] + i * g
+                h_n[layer_id] = o * torch.tanh(c_n[layer_id])
 
-print(f"ref h0  : {h0.shape}, {h0.min()}, {h0.max()}")
-print(f"cpp hn  : {hn.shape}, {hn.min()}, {hn.max()}")
-print(f"diff    : {(hn-h0).abs().max()}")
-print()
+            out[t] = h_n[self.num_layers - 1].clone()
 
-print(f"ref c0  : {c0.shape}, {c0.min()}, {c0.max()}")
-print(f"cpp cn  : {cn.shape}, {cn.min()}, {cn.max()}")
-print(f"diff    : {(cn-c0).abs().max()}")
-print()
+        return out, h_n, c_n
---
+++ python/test_lstm.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+
+torch.set_grad_enabled(False)
+
+torch.manual_seed(1)
+
+from multi_layer_lstm import MyLSTM
+
+seq_len = 22
+batch_size = 7
+input_size = 10
+hidden_size = 20
+num_layers = 5
+
+
+rnn = nn.LSTM(input_size, hidden_size, num_layers, batch_first=False, bidirectional=False)
+
+mylstm = MyLSTM(input_size, hidden_size, num_layers, rnn.state_dict())
+
+
+x = torch.randn(seq_len, batch_size, input_size)
+
+y0, (h0, c0) = rnn.forward(x)
+y1, hn, cn = mylstm.forward(x)
+
+# check outputs
+print(f"ref out : {y0.shape}, {y0.min()}, {y0.max()}")
+print(f"cpp out : {y1.shape}, {y1.min()}, {y1.max()}")
+print(f"diff    : {(y1-y0).abs().max()}")
+print()
+
+print(f"ref h0  : {h0.shape}, {h0.min()}, {h0.max()}")
+print(f"cpp hn  : {hn.shape}, {hn.min()}, {hn.max()}")
+print(f"diff    : {(hn-h0).abs().max()}")
+print()
+
+print(f"ref c0  : {c0.shape}, {c0.min()}, {c0.max()}")
+print(f"cpp cn  : {cn.shape}, {cn.min()}, {cn.max()}")
+print(f"diff    : {(cn-c0).abs().max()}")
+print()
